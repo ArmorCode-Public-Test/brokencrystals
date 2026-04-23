06f6bfd23b584354aeefdbf8da89c26b4e845a68
viram-armorcode <146802294+viram-armorcode@users.noreply.github.com>
2026-04-23 13:31:47 +0530

    testing sarif 4

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -57,8 +57,9 @@ export class EmailService {
     subject: string,
     body: string
   ) {
-      // intentional comment
     to = to.replace('\n', '%0A');
+    avar = from.replace('\n', '%0A');
+
     this.logger.debug(`Creating vulnerable mailOptions. "to" param is: ${to}`);
 
     let parsedSubject: string | RegExpExecArray | null = subject;
